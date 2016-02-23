c7d9529c0903fcf9177713c9551e33113d62fac2
Hossain <shakhawat.hossain@selise.ch>
2016-02-23 13:59:41 +0600

    the final python code

--- post.py
+++ post.py
@@ -2,15 +2,17 @@ import facebook
 token="CAAYGoBM5Q8gBACZCUh07FiZAH48Gjtj5FZAxgSKg6T38X75ZC49ZA5VRmZAMti9jJx5mklvlvI2C1PXR96Jbzop0QjXciprsRBKQ8xhb8dcbiVQIFjAyOr2LFtWTGJUwjzC4AEPzr4ZAFXl9mbKJMetVjZCOy2F5QUM32txyZAKZAZAZAGd2AyujqCgfZCOlDr8BpMF0VPEI9Ba1ayBTahj4ZCvGY6"
 graph =facebook.GraphAPI(token)
 PROFILE_ID ='me'
-attach = {
+#post a website link 
+'''attach = {
   "name": 'Hello world',
   "link": 'https://www.google.com',
   "caption": 'test post',
   "description": 'some test',
-  "picture" : 'download.jpg',
-}
+  "picture" :'https://www.google.com/download.jpg',
+}'''
 
-graph.put_wall_post('yap!!!!!!!!!!  Hello from Python ',profile_id = PROFILE_ID,attachment=attach )
+graph.put_photo(open('download.jpg'), message='Test photo upload https://www.google.com')
+#graph.put_wall_post('yap!!!!!!!!!!  Hello from Python ',profile_id = PROFILE_ID,attachment=attach)
 
 
 
