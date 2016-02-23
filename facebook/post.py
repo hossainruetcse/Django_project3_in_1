9ac5e8770008c11969403f15d651bc5fc5363e89
Hossain <Hossain.ruetcse@gmail.com>
2016-02-23 11:50:51 +0600

    this a application of facebook wall post using python SDK

---
+++ facebook/post.py
@@ -0,0 +1,15 @@
+import facebook
+token="CAAYGoBM5Q8gBACZCUh07FiZAH48Gjtj5FZAxgSKg6T38X75ZC49ZA5VRmZAMti9jJx5mklvlvI2C1PXR96Jbzop0QjXciprsRBKQ8xhb8dcbiVQIFjAyOr2LFtWTGJUwjzC4AEPzr4ZAFXl9mbKJMetVjZCOy2F5QUM32txyZAKZAZAZAGd2AyujqCgfZCOlDr8BpMF0VPEI9Ba1ayBTahj4ZCvGY6"
+graph =facebook.GraphAPI(token)
+PROFILE_ID ='me'
+graph.put_wall_post('yap!!!!!!!!!!  Hello from Python ',profile_id = PROFILE_ID )
+
+
+
+
+#access token time extend 
+'''https://graph.facebook.com/oauth/access_token?             
+client_id=APP_ID&
+client_secret=APP_SECRET&
+grant_type=fb_exchange_token&
+fb_exchange_token=EXISTING_ACCESS_TOKEN '''
